377813e3cc7c8ddd26f2c80c4634b0ba9db7ca62
Myslitel666 <tretyakov_02@bk.ru>
2025-03-18 19:57:51 +0500

    BUILD SUCCESSFUL WITH D.TS FILES!!!

---
+++ src/Hello.js
@@ -0,0 +1,5 @@
+import { jsxs as _jsxs } from "react/jsx-runtime";
+const Hello = ({ name = "World" }) => {
+    return (_jsxs("p", { children: ["Hello, ", name, "!"] }));
+};
+export default Hello;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+export default defineConfig({
+    plugins: [react(), dts()],
+    build: {
+        lib: {
+            entry: 'src/index.ts', // Точка входа в библиотеку
+            name: 'MyLib', // Глобальное имя для UMD (если понадобится)
+            formats: ['es', 'cjs'], // Собираем ESM и CJS
+            fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'], // Не включаем React в бандл
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+    },
+});
